6e93f08ddedc526af17934d21bdabddf5669cd7a
Abhiroop25902 <abhiroop.m25902@gmail.com>
2022-01-18 10:56:55 +0530

    [FIX] made coco_dict "private"

--- core/cocoLabelName.py
+++ core/cocoLabelName.py
@@ -2,14 +2,14 @@ FILENAME = './coco_labels.txt'
 
 class CocoLabelName:
     def __init__(self):
-        self.coco_dict = {}
+        self._coco_dict = {}
         for line in open(FILENAME, 'r').readlines():
             line = line.rstrip('\n')
             phrases = line.split(',')
             key = int(phrases[0].split(':')[1])
             value = phrases[1].split(':')[1]
-            self.coco_dict[key] = value
+            self._coco_dict[key] = value
 
 
     def getName(self, label: int) -> str:
-        return self.coco_dict[label]
+        return self._coco_dict[label]
