0ba01bbc2c0382b0feed3b3f6fd4e85b70dc01c2
Abhiroop25902 <abhiroop.m25902@gmail.com>
2022-01-18 14:14:00 +0530

    [NEW] decopled detector and drawBoxes

--- core/objectDetector.py
+++ core/objectDetector.py
@@ -5,30 +5,31 @@ import tensorflow_hub as hub
 # for calulating time per processing
 import time
 
-#for setting enviornment variables
+# for setting enviornment variables
 import os
-
-from .imageDrawer import draw_boxes
-from .imageUtils import save_img
 
 MODEL_HANDLE = "https://tfhub.dev/tensorflow/efficientdet/lite0/detection/1"
 
 # NOTE: uncomment this line to make the GPU invisible to tensorflow
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+
 class Detector:
     """Class wrapper for running object detector model inferences"""
+
     def __init__(self):
         """loads the detector model"""
         self.detector = hub.load(MODEL_HANDLE)
 
     def run_detector(self, img):
-        """run detector algorithm and save the image"""
-        #TODO Return Image
-        #TODO decouple prediction and bounding box drawing
+        """
+        Run detector algorithm and returns a dictionary with keys 
+        "detection_boxes", "detection_classes" and "detection_scores" 
+        """
 
-        #convert the image 
-        converted_img = tf.image.convert_image_dtype(img, tf.uint8)[tf.newaxis, ...]
+        # convert the image
+        converted_img = tf.image.convert_image_dtype(img, tf.uint8)[
+            tf.newaxis, ...]
 
         start_time = time.time()
         boxes, scores, classes, num_detections = self.detector(converted_img)
@@ -43,18 +44,11 @@ class Detector:
         print(f"Found {int(num_detections)} objects.")
         print(f"Inference time: {end_time-start_time} s")
 
-        image_with_boxes = draw_boxes(
-            img.numpy(),
-            result["detection_boxes"],
-            result["detection_classes"],
-            result["detection_scores"]
-        )
-
-        save_img(image_with_boxes)
+        return result
 
 
 if __name__ == "__main__":
-    # # Print Tensorflow version
+    #Print Tensorflow version
     print(tf.__version__)
 
     if tf.test.gpu_device_name():
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from core.imageUtils import download_and_resize_image, load_img
+from core.imageDrawer import draw_boxes
+from core.imageUtils import download_and_resize_image, load_img, save_img
 from core.objectDetector import Detector
 
 # By Heiko Gorski, Source: https://commons.wikimedia.org/wiki/File:Naxos_Taverna.jpg
@@ -9,4 +10,13 @@ if __name__ == '__main__':
     img = load_img(downloaded_image_path)
 
     detector = Detector()
-    detector.run_detector(img)
+    result = detector.run_detector(img)
+
+    image_with_boxes = draw_boxes(
+            img.numpy(),
+            result["detection_boxes"],
+            result["detection_classes"],
+            result["detection_scores"]
+        )
+
+    save_img(image_with_boxes)
