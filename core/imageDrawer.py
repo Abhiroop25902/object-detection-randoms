c02f90d2280408f3b08677e5c229356a7eb8d9c7
Abhiroop25902 <abhiroop.m25902@gmail.com>
2022-01-18 14:05:40 +0530

    [FIX] draw_bounding_box_.. accpets string not list

--- core/imageDrawer.py
+++ core/imageDrawer.py
@@ -18,19 +18,18 @@ def draw_bounding_box_on_image(
     xmax: float,
     color: str,
     font: ImageFont,
-    thickness: int = 4,
-    display_str_list=()
+    display_str: str = "",
+    thickness: int = 4
 ):
-    """Adds a bounding box to an image."""
-    # TODO make display_str_list simpler -> why is there a empty tuple there?
+    """Adds a bounding box to an image. caling this function changes the passed image itself"""
 
     # draw is an editable image where we can draw stuffs, any changes in draw also gets reflected in the image
     draw = ImageDraw.Draw(image)
-    # im_width, im_height = image.size
 
-    # xmin, xmax, ymin, ymax -> values between 0 and 1 according to whole image resolution
+    # xmin, xmax, ymin, ymax are float values depicting pixels of the image, so rounding them to int
     (left, right, top, bottom) = tuple(
-        map(lambda val: round(float(val)), [xmin, xmax, ymin, ymax]))
+        map(lambda val: round(float(val)), [xmin, xmax, ymin, ymax])
+    )
 
     # draw rectangle in the bounding box
     draw.line(
@@ -40,45 +39,35 @@ def draw_bounding_box_on_image(
         fill=color
     )
 
-    # display_str_list is the list of possible objects detected by the object detection model
-
     # If the total height of the display strings added to the top of the bounding
     # box exceeds the top of the image, stack the strings below the bounding box
     # instead of above.
-    display_str_heights = [font.getsize(dis_str)[1]
-                           for dis_str in display_str_list]
-    # Each dis_str has a top and bottom margin of 0.05x.
-    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+    text_width, text_height = font.getsize(display_str)  # 0th element is width
+    # display_str has a top and bottom margin of 0.05x.
+    margin = np.ceil(0.05 * text_height)
+    total_display_str_height = text_height + 2*margin
 
     if top > total_display_str_height:
         text_bottom = top
     else:
         text_bottom = top + total_display_str_height
 
-    # Reverse list and print from bottom to top.
-    for display_str in display_str_list[::-1]:
-        text_width, text_height = font.getsize(display_str)
-        margin = np.ceil(0.05 * text_height)
+    # generate background for the text
+    draw.rectangle(
+        [
+            (left, text_bottom - text_height - 2 * margin),  # top left
+            (left + text_width, text_bottom)  # bottom right
+        ],
+        fill=color
+    )
 
-        # generate background for the text
-        draw.rectangle(
-            [
-                (left, text_bottom - text_height - 2 * margin),  # top left
-                (left + text_width, text_bottom)  # bottom right
-            ],
-            fill=color
-        )
-
-        # draw text
-        draw.text(
-            (left + margin, text_bottom - text_height - margin),  # bottom left
-            display_str,
-            fill="black",
-            font=font
-        )
-
-        # repeat in case of multiple detection model
-        text_bottom -= text_height - 2 * margin
+    # draw text
+    draw.text(
+        (left + margin, text_bottom - text_height - margin),  # bottom left
+        display_str,
+        fill="black",
+        font=font
+    )
 
 
 def draw_boxes(image, boxes, class_labels, scores, max_boxes=20, min_score=0.1):
@@ -88,11 +77,13 @@ def draw_boxes(image, boxes, class_labels, scores, max_boxes=20, min_score=0.1):
 
     try:
         font = ImageFont.truetype(
-            "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 25)
+            "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 25
+        )
     except IOError:
         print("Font not found, using default font.")
         font = ImageFont.load_default()
 
+    # for converting cocoLabel to object names
     cocoLabelName = CocoLabelName()
 
     for i in range(min(boxes.shape[0], max_boxes)):
@@ -109,8 +100,7 @@ def draw_boxes(image, boxes, class_labels, scores, max_boxes=20, min_score=0.1):
             color = colors[hash(object_name) % len(colors)]
             image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
             draw_bounding_box_on_image(
-                image_pil, ymin, xmin, ymax, xmax, color, font, display_str_list=[
-                    display_str]
+                image_pil, ymin, xmin, ymax, xmax, color, font, display_str
             )
             np.copyto(image, np.array(image_pil))
     return image
