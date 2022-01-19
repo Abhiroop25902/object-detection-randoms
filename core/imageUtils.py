55f811ff28fed299ec10f84e08ab802a594617e5
Abhiroop25902 <abhiroop.m25902@gmail.com>
2022-01-19 14:58:41 +0530

    [EDIT] changed PIL function calls with opencv

--- core/imageUtils.py
+++ core/imageUtils.py
@@ -1,56 +1,57 @@
-# for image IO read and decode
-import tensorflow as tf
+# for image write and read, resizing image
+import cv2
 
-# for images save
-import matplotlib.pyplot as plt
-
-#for making image file in /tmp
+# for making image file in /tmp
 import tempfile
 
 # for downloading images from a given link
-from six.moves.urllib.request import urlopen
-from six import BytesIO
+import urllib
+import numpy as np
 
-# for converting input photo to array form 
-from PIL import Image
-
-#for resizing images
-from PIL import ImageOps
 
 def load_img(path: str):
-    img = tf.io.read_file(path)
-    img = tf.image.decode_jpeg(img, channels=3)
+    img = cv2.imread(path)
+    # NOTE: cv2.imread reads in BGR format, so we need to change it to RGB
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
 
-def save_img(image, outputFileName='./output.jpg'):
-    plt.imsave(outputFileName, image)
+def save_img(img, outputFileName='./output.jpg'):
+    # NOTE: img saved will be read as BRG format, we changed it to RGB in
+    # load_img, so we need to convert RGB to BRG
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(outputFileName, img)
     print(f"Images is saved as {outputFileName}")
 
 
-def download_and_resize_image(url: str, new_width: int = 256, new_height: int = 256) -> str:
-    """downloads and saves the image on a temporary file, return the path of that file"""
+def download_and_resize_image(
+    url: str,
+    new_width: int = 256,
+    new_height: int = 256
+) -> str:
+    """
+    downloads and saves the image on a temporary file,
+    return the path of that file
+    """
     # make a temp file to download and store the images
     _, filename = tempfile.mkstemp(suffix=".jpg")
 
-    # download the image
-    response = urlopen(url)
-    image_data = response.read()
-    image_data = BytesIO(image_data)
-    # image is now 0 and 1 data
-    # convert image to matrix form
-    pil_image = Image.open(image_data)
-    # resize the image
-    pil_image = ImageOps.fit(
-        pil_image, (new_width, new_height),
-        Image.ANTIALIAS
-    )
+    with urllib.request.urlopen(url) as response:
+        # read image as an numpy array
+        image = np.asarray(bytearray(response.read()), dtype="uint8")
 
-    # if image is in some different format, convert to rgb
-    pil_image_rgb = pil_image.convert("RGB")
+    # convert numpy image array to cv2 image
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    # resize the image
+    image = cv2.resize(
+        image,
+        (new_width, new_height)
+    )
+    # NOTE: img is in BGR format and imwrite also reads it in BRG format
 
     # save file to the temporary file made
-    pil_image_rgb.save(filename, format="JPEG", quality=90)
+    cv2.imwrite(filename, image)
     print("Image downloaded to %s." % filename)
 
     return filename
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from core.imageDrawer import draw_boxes
 from core.imageUtils import download_and_resize_image, load_img, save_img
 from core.objectDetector import Detector
 
-# By Heiko Gorski, Source: https://commons.wikimedia.org/wiki/File:Naxos_Taverna.jpg
+# By Heiko Gorski
+# Source: https://commons.wikimedia.org/wiki/File:Naxos_Taverna.jpg
 IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Naxos_Taverna.jpg/800px-Naxos_Taverna.jpg"
 
 if __name__ == '__main__':
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     result = detector.run_detector(img)
 
     image_with_boxes = draw_boxes(
-        img.numpy(),
+        img,
         result["detection_boxes"],
         result["detection_classes"],
         result["detection_scores"]
